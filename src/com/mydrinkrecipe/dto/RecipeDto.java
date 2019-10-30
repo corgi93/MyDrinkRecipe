cbc735d450e279228fbdaf2ea9e6f27ab842837d
ShiningJean <gurwls714@naver.com>
2019-10-30 12:17:35 +0900

    corgi - RecipeDto.jsp 생성
    RecipeDto getter/setter 작성

--- src/com/mydrinkrecipe/dto/RecipeDto.java
+++ src/com/mydrinkrecipe/dto/RecipeDto.java
@@ -1,14 +1,86 @@
 package com.mydrinkrecipe.dto;
 
 public class RecipeDto {
-	private int r_num;
-	private String r_name;
-	private String r_content;
-	private String r_img;
-	private int r_price;
-	private int r_time;
-	private int r_kcal;
-	private int r_readcount;
-	private int r_category;
+	private int bno;
+	private String title;
+	private String content;
+	private String ingredient;
+	private String time;
+	private String price;
+	private String likecount;
+	private String kcal;
+	private String img;
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(String ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getLikecount() {
+		return likecount;
+	}
+
+	public void setLikecount(String likecount) {
+		this.likecount = likecount;
+	}
+
+	public String getKcal() {
+		return kcal;
+	}
+
+	public void setKcal(String kcal) {
+		this.kcal = kcal;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
 }
-	
