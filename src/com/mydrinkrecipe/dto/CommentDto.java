761ef5764f48d26c2d545e4a9986bfe1db8f7a1c
Eddington524 <iamsam8687@gmail.com>
2019-11-04 02:42:46 +0900

    Eddington-CommentDto수정
    bno를 comment bno로 수정하고 타입을 int에서 string으로 수정했습니다.

--- src/com/mydrinkrecipe/dto/CommentDto.java
+++ src/com/mydrinkrecipe/dto/CommentDto.java
@@ -3,21 +3,21 @@ package com.mydrinkrecipe.dto;
 import java.sql.Timestamp;
 
 public class CommentDto {
-	private int bno;
-	private int recipe_bno;
+	private String comment_bno;
+	private String recipe_bno;
 	private String content;
 	private Timestamp writeday;
 	
-	public int getBno() {
-		return bno;
+	public String getComment_bno() {
+		return comment_bno;
 	}
-	public void setBno(int bno) {
-		this.bno = bno;
+	public void setComment_bno(String comment_bno) {
+		this.comment_bno = comment_bno;
 	}
-	public int getRecipe_bno() {
+	public String getRecipe_bno() {
 		return recipe_bno;
 	}
-	public void setRecipe_bno(int recipe_bno) {
+	public void setRecipe_bno(String recipe_bno) {
 		this.recipe_bno = recipe_bno;
 	}
 	public String getContent() {
