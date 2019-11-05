c29ea7fc01abe76ce8edcb0784bb7db93f27b6f7
Eddington524 <iamsam8687@gmail.com>
2019-11-05 15:23:22 +0900

    eddington-detailComment ajax 추가
    ajax추가했습니다!%^^&

---
+++ WebContent/resources/js/detailComment.js
@@ -0,0 +1,88 @@
+   $(function() {
+      //처음 시작시 댓글 리스트 출력
+      list();
+      
+      $("#btnsave").click(function(){
+         
+         var content = $("#content").val();
+         var recipe_bno = $("#recipe_bno").val();
+         var nick = $("#nick").val();
+         var user_img=$("#user_img").val();
+         
+         if(content==''){
+            alert("내용을 반드시 입력해주세요");
+            return false;
+         }
+         
+         $.ajax({
+            type:"post",
+            url:"controllers/comments/commentAction.jsp",
+            dataType:"xml",
+            data:{
+               "content":content,
+               "recipe_bno" : recipe_bno,
+               "nick" : nick,
+               "user_img":user_img,
+            },
+            success:function(data){
+               //var d = data.find("commentdata").text();
+               //var s=$(this);
+
+               $("#content").val("");
+               $("#content").focus();
+               list();
+            },
+             statusCode : {
+                     404: function(){
+                        alert("url을 찾을수 없어요");
+                     },
+                     500: function(){
+                        alert("서버 오류");
+                     }
+                  }
+         });
+      });
+   });
+   
+   function list()
+   {
+   //데이터 불러오기
+   $.ajax({
+      type:"post",
+      url:"controllers/comments/commentList.jsp",
+      data:{"recipe_bno":$("#bno").val()},//자바 코드로 주지말고 위에 input type
+      success:function(data){
+         $("#content").val("");
+         $("#content").focus();
+         var str="";
+         ////초기화 코드가 each문 아래 있으면 반복으로 돌면서 계속 초기화 되니까 위에 있어야됨!
+      $(data).find("commentdata").each(function(){
+         
+         var s=$(this);
+         str+="<div class='old_comment'>"
+         str+="<span class='answer_profil_img'>";
+         str+="<img src="+"'"+s.find("memberimg").text()+"'>";
+         str+="</span>";
+         str+="<div style='width:600px;'>";
+         str+="<b>"+s.find("nick").text()+"</b>";
+         str+="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+         str+="<writeday>"+s.find("writeday").text()+"</writeday>";
+         str+="</div>";
+         str+="<div style='width:600px;'>";
+         str+=s.find("content").text();
+         str+="</div>";
+         str+="</div>";
+      });
+      
+      $("#out1").html(str);
+      },
+      statusCode:{
+             404:function(){
+                    alert("url 을 찾을수 없어요");
+             },
+             500:function(){
+                    alert("서버 오류");
+             }
+       }
+   });
+   }
